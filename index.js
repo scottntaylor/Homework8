774e64209a3e90c929fb0da21029b613484ae559
Scott Taylor <scottntaylor21@gmail.com>
2019-11-21 18:17:23 -0800

    prompt navigation complete

--- index.js
+++ index.js
@@ -3,76 +3,88 @@ var inquirer = require('inquirer');
 
 managerPrompt();
 
-function managerPrompt(){
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is the name of your manager?",
-            name: "managerName",
-        },
-        {
-            type: "input",
-            message: "What is your ID number?",
-            name: "idNumber",
-        },
-        {
-            type: "input",
-            message: "What is your email address?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "What is your office number?",
-            name: "office",
-        },
-
-
-
-    ])
-    .then(answers => {
-
-        var managerInfo = {
-            managerName: answers.managerName,
-            id: answers.idNumber,
-            email: answers.email,
-            office: answers.office,
-
-        }
-        console.log(managerInfo.id);
-      
-
-        memberQuestion();
-
-        
-    });
-}
-
-    function memberQuestion(){
-inquirer
-    .prompt([
-        {
-            type: "list",
-            message: "Would you like to add a team member?",
-            name: "teamMember",
-            choices: ["Engineer", "Intern", "I am finished"]
-        },
-    ])
-
-    .then(answers =>{
-        console.log(answers.teamMember);
-        let memberInfo = {
-            memberType: answers.teamMember,
-        }
-    })
-}
-
-function engineerPrompts(){
+function managerPrompt() {
     inquirer
         .prompt([
             {
                 type: "input",
-                message: "What is your?",
+                message: "What is the name of your manager?",
+                name: "managerName",
+            },
+            {
+                type: "input",
+                message: "What is your ID number?",
+                name: "idNumber",
+            },
+            {
+                type: "input",
+                message: "What is your email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What is your office number?",
+                name: "office",
+            },
+
+
+
+        ])
+        .then(answers => {
+
+            var managerInfo = {
+                managerName: answers.managerName,
+                id: answers.idNumber,
+                email: answers.email,
+                office: answers.office,
+
+            }
+
+
+
+            memberQuestion();
+
+
+        });
+}
+
+function memberQuestion() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to add a team member?",
+                name: "teamMember",
+                choices: ["Engineer", "Intern", "I am finished"]
+            },
+        ])
+
+        .then(answers => {
+            // console.log(answers.teamMember);
+
+            var memberInfo = {
+                memberType: answers.teamMember,
+            }
+        
+    if (memberInfo.memberType === "Engineer") {
+        engineerPrompts();
+    }
+    else if (memberInfo.memberType === "Intern") {
+        internPrompts();
+    }
+     else if (memberInfo.memberType === "I am finished"){
+    // Run Create HTML Function
+     console.log("Your team member display is ready!")
+     }
+})
+}
+
+function engineerPrompts() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your name?",
                 name: "memberName",
             },
             {
@@ -90,55 +102,57 @@ function engineerPrompts(){
                 message: "what is your Github username?",
                 name: "github",
             }
-            
+
 
         ])
-        .then(answers =>{
-            console.log(answers);
-            
+        .then(answers => {
+            // console.log(answers);
+
             let engineerInfo = {
                 name: answers.memberName,
                 id: answers.idNumber,
                 email: answers.email,
                 github: answers.github,
             }
+            memberQuestion();
         })
 
 };
-function internPrompts(){
+function internPrompts() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is your?",
-            name: "memberName",
-        },
-        {
-            type: "input",
-            message: "What is your ID number?",
-            name: "idNumber",
-        },
-        {
-            type: "input",
-            message: "What is your email address?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "what is your college/university?",
-            name: "college",
-        }
-        
+        .prompt([
+            {
+                type: "input",
+                message: "What is your name?",
+                name: "memberName",
+            },
+            {
+                type: "input",
+                message: "What is your ID number?",
+                name: "idNumber",
+            },
+            {
+                type: "input",
+                message: "What is your email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "what is your college/university?",
+                name: "college",
+            }
 
-    ])
-    .then(answers =>{
-        console.log(answers);
 
-        let internInfo = {
-            name: answers.name,
-            id: answers.idNumber,
-            email: answers.email,
-            college: answers.college;
-        }
-    })
+        ])
+        .then(answers => {
+            // console.log(answers);
+
+            let internInfo = {
+                name: answers.name,
+                id: answers.idNumber,
+                email: answers.email,
+                college: answers.college,
+            }
+            memberQuestion();
+        })
 }
