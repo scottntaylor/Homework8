ece46dce0ae0694e7d1a04673f45e5233d90d1b6
Scott Taylor <scottntaylor21@gmail.com>
2019-11-21 17:53:20 -0800

    engineer and intern prompts

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var inquirer = require('inquirer');
 
+
+managerPrompt();
+
+function managerPrompt(){
 inquirer
     .prompt([
         {
@@ -23,17 +27,11 @@ inquirer
             name: "office",
         },
 
-        {
-            type: "list",
-            message: "Woudl you like to add a team member?",
-            name: "teamMember",
-            choices: ["Engineer", "Intern", "I am finished"]
-        },
+
 
     ])
     .then(answers => {
-        // console.info('searching....');
-        // console.log(answers);
+
         var managerInfo = {
             managerName: answers.managerName,
             id: answers.idNumber,
@@ -41,6 +39,89 @@ inquirer
             office: answers.office,
 
         }
-console.log(managerInfo.id);
-        // gatherData(answers);
+        console.log(managerInfo.id);
+      
+
+        memberQuestion();
+
+        
     });
+}
+
+    function memberQuestion(){
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Would you like to add a team member?",
+            name: "teamMember",
+            choices: ["Engineer", "Intern", "I am finished"]
+        },
+    ])
+
+    .then(answers =>{
+        console.log(answers.teamMember);
+    })
+}
+
+function engineerPrompts(){
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your?",
+                name: "memberName",
+            },
+            {
+                type: "input",
+                message: "What is your ID number?",
+                name: "idNumber",
+            },
+            {
+                type: "input",
+                message: "What is your email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "what is your Github username?",
+                name: "github",
+            }
+            
+
+        ])
+        .then(answers =>{
+            console.log(answers);
+        })
+
+};
+function internPrompts(){
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your?",
+            name: "memberName",
+        },
+        {
+            type: "input",
+            message: "What is your ID number?",
+            name: "idNumber",
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "what is your college/university?",
+            name: "college",
+        }
+        
+
+    ])
+    .then(answers =>{
+        console.log(answers);
+    })
+}
